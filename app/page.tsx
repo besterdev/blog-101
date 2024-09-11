cb76704438c77930aa8aab195c2d5eca5ca50224
Thawatchai Kraiwitwattana <Thawatchai.krai@gmail.com>
2024-09-11 22:12:56 +0700

    🚀 Add: implement kinde

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-export default function Home() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
+
+const Home = async () => {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
   return (
     <main>
       <h1>Hello Blog 101</h1>
+      {user ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ) : (
+        <div>
+          <LoginLink>
+            <Button>Sign in</Button>
+          </LoginLink>
+          <RegisterLink>
+            <Button>Sign up</Button>
+          </RegisterLink>
+        </div>
+      )}
     </main>
   );
-}
+};
+
+export default Home;
